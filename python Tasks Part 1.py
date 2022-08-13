1d2dffa9220d259a504df526dbca05d42cae89b9
AbdulazizAlenezi <66471363+agueroq8@users.noreply.github.com>
2022-08-13 22:39:32 +0300

    task part1

---
+++ python Tasks Part 1.py
@@ -0,0 +1,58 @@
+#Python Tasks
+dash = "---------------------------------------"
+print("Create a Boolean variable named x")
+x = True
+print("x = ",x ," ,Type = ",type(x))
+
+print(dash)
+
+print( "Create an integer variable named y ")
+y = 0
+print("y = " ,y ," ,Type = ",type(y))
+
+print(dash)
+
+print("Create a float variable named z ")
+z = 0.0
+print("z = " , z ," ,Type = ", type(z))
+
+print(dash)
+
+print("Create a string variable names s")
+s = "pyhton"
+print("s = " ,s ," ,Type = ", type(s))
+
+print(dash)
+
+print("Can we convert the str to int ?")
+o = "o"
+print(" o is = ",type(o))
+o = 0
+print("o now is ",type(o))
+
+print(dash)
+
+print("Create a list of numbers from 1 to 5")
+mylist = [1,2,3,4,5]
+print(mylist , "type = " ,type(mylist))
+
+
+print(dash)
+
+print(" Convert the list to tuple")
+mylist = (1,2,3,4,5)
+print(mylist,"type = ",type(mylist))
+
+print(dash)
+
+print(" Create a dict of 3 values")
+mydic = { "aziz":1 ,
+          "ahmad":2,
+          "khaled":3}
+print(mydic)
+
+print(dash)
+
+print(" Can we use semi colon ; with python")
+print(" Yes we Can Use it to multiline Commands")
+
