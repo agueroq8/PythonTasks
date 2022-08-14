9d74c221eef8ada74af4d4f4dd4e497576a8c965
AbdulazizAlenezi <66471363+agueroq8@users.noreply.github.com>
2022-08-14 16:33:18 +0300

    Complete all the Task

--- python Tasks Part 1.py
+++ python Tasks Part 1.py
@@ -1,7 +1,8 @@
 #Python Tasks
 dash = "---------------------------------------"
 print("Create a Boolean variable named x")
-x = True
+x = True
+
 print("x = ",x ," ,Type = ",type(x))
 
 print(dash)
@@ -53,6 +54,153 @@ print(mydic)
 
 print(dash)
 
-print(" Can we use semi colon ; with python")
+print(" Can we use semi colon ; with python ?")
 print(" Yes we Can Use it to multiline Commands")
 
+print(dash)
+
+print(" Python is interpreted or compiled ?")
+print(" its interpreted , works line by line its help with big project make it smooth")
+
+print(dash)
+
+print(" What is the differences between low level & high level")
+print(" low level its tools for programming languages close to meachene language less translate /n high level tools programming languages close to english easy to understand slower than low level")
+
+print(dash)
+
+print(" What is the operator precedence")
+print(" (), * , / ,+ , - ")
+
+print(dash)
+
+print("Create a variable x with value of 10")
+x = 10
+print(x)
+
+print("Increase x value by 15 using assignment operators ")
+x += 15
+print(x)
+
+print(dash)
+
+print("Divide the x value by 5 using assignment operators")
+x /=5
+print(x)
+
+print(dash)
+
+print("Multiply the x value by 10 using assignment operator")
+x *=10
+print(x)
+
+print(dash)
+
+print(" Get module of x on 3 using assignment operators")
+print(" I dont UnderStand this ^")
+
+print(dash)
+
+print("Using python print the module of 11 to 4")
+print(" I dont UnderStand this ^")
+
+print(dash)
+
+print("Print the exponent of 2,3")
+print("2**3")
+
+print(dash)
+
+print("Divide 11 by 3 using python division")
+div = 11/3
+print(div)
+
+print(dash)
+
+print("Can we divide 11 by 3 and get an integer number ?")
+div = 11//3
+print(div)
+
+print(dash)
+
+print("Check if 10 is bigger than 15 or not ")
+var =  10 > 15
+print("10 > 15 = " ,var)
+
+print(dash)
+
+print("If 10 is not bigger than15 print x is smaller than 15")
+x=10
+y = 15
+if x < y :
+    print(" x  is smaller than 15")
+
+print(dash)
+
+print("In which cases we will use all")
+print(" We use all for all conditions are true ")
+
+print(dash)
+
+print("What is the differences between all , and")
+print(" same purpose , all is function we use it for too many ands")
+
+print(dash)
+
+print(" What is the differences between any , or ")
+print(" same purpose , any is function we use it for too many ors")
+
+print(dash)
+
+print("If we need all the conditions to be true we will use …")
+print(" all , and")
+
+print(dash)
+
+print("What is the differences between if , elif")
+print(" we use if statmeant if i have condition , elif if i have more conditions")
+
+print(dash)
+
+print(" What is the differences between elif else")
+print(" else uses if none of if or elif conditions are true ")
+
+print(dash)
+
+print(" Can we use more than one elif")
+print(" Yes We can use unlimted elf ")
+
+print(dash)
+
+print("Can we use more than one else ")
+print("its last thing in if conditions just once")
+
+print(dash)
+
+print("write s simple ternary operator")
+print("'you are adult' if age >18 else 'you are under age' << in this example we will use 10 for age " )
+age = 10
+print('you are adult' if age >18 else 'you are under age')
+
+print(dash)
+
+print(" in elif , python will check all the conditions no matter what ?")
+print(" no until find elif is true will skip the others elif")
+
+print(dash)
+
+print(" in elif we use else for ... ?")
+print(" else for last conditon ")
+
+print(dash)
+
+print("if we have this list [2,4,6,8,10] :\n○ check to see if 2 , 4 and 5 in this list")
+MyList =[2,4,6,8,10]
+print("○ check to see if 4 in this list or not")
+print(" 4 in MyList = ",4 in MyList)
+print("○ check to see if 4 and 6 in this list on not ")
+print(" 4 and 6 in Mylist = ", 4 and 6 in MyList)
+print("○ check to see if 3 or 6 in this list ")
+print(" 3 or 6 in MyList = " , 6 or 3 in MyList)
+print("○ check to see if 2 , 4 and 5 in this list")
+print(" all(2,4,5) in MyList or  2 and 4 and 5 in MyList  = " , 2 and 4 and 5 in MyList)
